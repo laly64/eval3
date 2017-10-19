26091ef150b1ddbfc6780a7e72feaab38a6764e7
laly64 <avalos53laly@gmail.com>
2017-10-19 19:44:38 -0300

    formulario
    Aqui se guardaron los cambios de, nombre, apellido y correo.

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -13,4 +13,19 @@ class Welcome extends CI_Controller {
 	{
 		$this->load->view('formulario');
 	}
+
+	function validar()
 }
+//echo "Hola<br>";
+		//Aqui estamos recibiendo los datos del formulario eva1.php.
+		$name = $this->input->post('nombre');
+		$lastname = $this->input->post('apellido');
+
+		echo "Nombre: ".$name."  Apellido: ".$lastname;
+
+		//aqui estamos creando el array
+		$data = array('nombre' => $this->input->post('nombre1'),'apellido' => $this->input->post('apellido1') );
+
+		//Aqui se llama la funcion para guardar en la BD
+		$this->form_model->guardar($data);
+		//}
